bd026a281aeae4f462742db4b43f185fed509976
Bryan Bugyi <bryanbugyi34@gmail.com>
2022-01-01 11:26:32 -0500

    Fix lru_cache test bug the right way

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,8 @@ from typing import Iterator
 from freezegun import freeze_time
 from pytest import fixture
 
+from clack import _dynvars as dyn
+
 
 pytest_plugins = ["logrus.pytest_plugin"]
 
@@ -17,3 +19,15 @@ def frozen_time() -> Iterator[None]:
     """Freeze time until our tests are done running."""
     with freeze_time("2021-09-06T15:45:03.585481Z"):
         yield
+
+
+@fixture(autouse=True)
+def clear_lru_cache() -> None:
+    """Clear function LRU cache before each test.
+
+    Clears cache of functions that have been decorated with
+    @functools.lru_cache before each test.
+    """
+
+    dyn.get_app_name.cache_clear()
+    dyn.get_config_defaults.cache_clear()
