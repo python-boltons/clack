4a48ec6475ed36731e6931870d222cbb1742f3fa
Bryan Bugyi <bryanbugyi34@gmail.com>
2022-01-04 08:29:33 -0500

    Bump version: 0.1.3 → 0.2.0

--- src/clack/__init__.py
+++ src/clack/__init__.py
@@ -28,6 +28,6 @@ __all__ = [
 
 __author__ = "Bryan M Bugyi"
 __email__ = "bryanbugyi34@gmail.com"
-__version__ = "0.1.3"
+__version__ = "0.2.0"
 
 _logging.getLogger(__name__).addHandler(_logging.NullHandler())
