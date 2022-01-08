c0e7e8c2ea4766a065f1df9619df3d127b22f16d
Bryan Bugyi <bryanbugyi34@gmail.com>
2022-01-08 17:27:37 -0500

    Bump version: 0.2.5 → 0.2.6

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ SUPPORTED_PYTHON_VERSIONS = [
     (3, 9),
     (3, 10),
 ]
-USE_SCM_VERSION = {"fallback_version": "0.2.5"}
+USE_SCM_VERSION = {"fallback_version": "0.2.6"}
 
 
 ###############################################################################
--- src/clack/__init__.py
+++ src/clack/__init__.py
@@ -30,6 +30,6 @@ __all__ = [
 
 __author__ = "Bryan M Bugyi"
 __email__ = "bryanbugyi34@gmail.com"
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 
 _logging.getLogger(__name__).addHandler(_logging.NullHandler())
